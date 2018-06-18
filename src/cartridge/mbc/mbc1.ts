5b53b011a953cdd22d8155299619ef3f7ae20d62
Lyrkan <twentyhundredounet@gmail.com>
2018-06-18 20:34:25 +0200

    Fix MBC1 ROM/RAM mode mechanism

--- src/cartridge/mbc/mbc1.ts
+++ src/cartridge/mbc/mbc1.ts
@@ -20,57 +20,37 @@ export class MBC1 implements IGameCartridgeMBC {
     this.currentRomBank = 1;
     this.currentRamBank = 0;
 
-    this.enabledRam = true;
+    this.enabledRam = false;
     this.romMode = true;
 
-    // Create a trap that will be called when something
-    // tries to write into the ROM banks.
-    const romWriteTrap = (bankIndex: number, offset: number, value: number) => {
-      // Only keep the 8 lower bits so we have the
-      // same behavior between .byte and .word
-      value = value & 0xFF;
-
-      // Change the behavior based on whether the
-      // write occurs on the static or the switchable
-      // banks.
-      if (bankIndex === 0) {
-        if (offset < 0x2000) {
-          // Enable/disable RAM
-          this.enabledRam = (value & 0b1111) === 0x0A;
-        } else if (offset < 0x4000) {
-          // ROM Bank switch (lower 5 bits)
-          // If the value is equal to 0x00 it is changed
-          // to 0x01.
-          this.currentRomBank = (this.currentRomBank & ~0b11111) | ((value || 1) & 0b11111);
-        }
-      } else {
-        if (offset < 0x2000) {
-          // RAM Bank switch / bits 6 and 7 of ROM bank
-          if (this.enabledRam) {
-            this.currentRamBank = (value & 0b11);
-          } else {
-            this.currentRomBank = (this.currentRomBank & ~(0b11 << 5)) | ((value & 0b11) << 5);
-          }
-        } else if (offset < 0x4000) {
-          // Enable/disable ROM mode
-          this.romMode = !(value & 1);
-
-          // If we are in RAM mode, remove bits 6 and 7
-          // of the current ROM bank.
-          if (!this.romMode) {
-            this.currentRomBank = this.currentRomBank & ~(0b11 << 5);
-          }
-        }
-      }
-    };
-
-    this.romBanks = romBanks.map((bank, index) => new MemorySegmentDecorator(bank, {
+    this.romBanks = romBanks.map((bank, bankIndex) => new MemorySegmentDecorator(bank, {
       setByte: (decorated, offset, value) => {
         if (offset < 0 || offset >= CARTRIDGE_ROM_BANK_LENGTH) {
           throw new RangeError(`Invalid address "${offset}"`);
         }
 
-        romWriteTrap(index, offset, value);
+        // Change the behavior based on whether the
+        // write occurs on the static or the switchable
+        // banks.
+        if (bankIndex === 0) {
+          if (offset < 0x2000) {
+            // Enable/disable RAM
+            this.enabledRam = (value === 0x0A);
+          } else if (offset < 0x4000) {
+            // ROM Bank switch (lower 5 bits)
+            // If the value is equal to 0x00 it is changed
+            // to 0x01.
+            this.currentRomBank = (this.currentRomBank & ~0b11111) | ((value & 0b11111) || 1);
+          }
+        } else {
+          if (offset < 0x2000) {
+            // RAM Bank switch / bits 6 and 7 of ROM bank
+            this.currentRamBank = (value & 0b11);
+          } else if (offset < 0x4000) {
+            // Enable/disable ROM mode
+            this.romMode = (value === 0);
+          }
+        }
       }
     }));
 
@@ -80,12 +60,25 @@ export class MBC1 implements IGameCartridgeMBC {
           throw new RangeError(`Invalid address "${offset}"`);
         }
 
+        // Always return 0xFF if RAM isn't enabled
         if (!this.enabledRam) {
           return 0xFF;
         }
 
         return decorated.getByte(offset);
-      }
+      },
+      setByte: (decorated, offset, value) => {
+        if (offset < 0 || offset >= CARTRIDGE_RAM_BANK_LENGTH) {
+          throw new RangeError(`Invalid address "${offset}"`);
+        }
+
+        // Don't write anything if RAM isn't enabled
+        if (!this.enabledRam) {
+          return;
+        }
+
+        return decorated.setByte(offset, value);
+      },
     }));
   }
 
@@ -98,13 +91,18 @@ export class MBC1 implements IGameCartridgeMBC {
     // Bank #0 cannot be accessed using the switchable bank
     // addresses since it is always available from 0x0000 to
     // 0x3FFF.
-    const index = (this.currentRomBank > 0) ? this.currentRomBank : 1;
+    let index = (this.currentRomBank > 0) ? this.currentRomBank : 1;
+
+    // If in ROM mode, use the current RAM bank index for bits 5 and 6
+    if (this.romMode) {
+      index |= (this.currentRamBank & 0b11) << 5;
+    }
+
     return this.romBanks[index];
   }
 
   public get ramBank() {
-    // Return the current RAM bank no matter what (disabled RAM
-    // is handled by a MemorySegmentDecorator).
-    return this.ramBanks[this.currentRamBank];
+    // Only RAM bank 0 is returned in ROM mode
+    return this.romMode ? this.ramBanks[0] : this.ramBanks[this.currentRamBank];
   }
 }
