9afb665fe4f3b13ebd6e38677fd75e70d1ee4e37
Lyrkan <twentyhundredounet@gmail.com>
2018-08-26 16:56:11 +0200

    Improve TerminalRenderer code style

--- src/display/renderers/terminal-renderer.ts
+++ src/display/renderers/terminal-renderer.ts
@@ -29,40 +29,38 @@ export class TerminalRenderer {
       process.stdout.write(RESET);
     }
 
-    const lastBg = new RgbColor(-1, -1, -1);
-    const lastFg = new RgbColor(-1, -1, -1);
+    const lastUpper = new RgbColor(-1, -1, -1);
+    const lastLower = new RgbColor(-1, -1, -1);
 
-    const currentBg = new RgbColor(0, 0, 0);
-    const currentFg = new RgbColor(0, 0, 0);
+    const currentUpper = new RgbColor(-1, -1, -1);
+    const currentLower = new RgbColor(-1, -1, -1);
 
     for (let y = 0; y < SCREEN_HEIGHT; y += 4) {
       for (let x = 0; x < SCREEN_WIDTH; x += 2) {
-        currentBg.reset();
-        currentFg.reset();
+        currentUpper.update(0, 0, 0);
+        currentLower.update(0, 0, 0);
 
         for (let subY = 0; subY < 2; subY++) {
           for (let subX = 0; subX < 2; subX++) {
-            const bgOffset = ((y + subY) * SCREEN_WIDTH * 3) + (x * 3);
-            const fgOffset = ((y + (subY * 2)) * SCREEN_WIDTH * 3) + (x * 3);
+            const upperOffset = ((y + subY) * SCREEN_WIDTH * 3) + (x * 3);
+            const lowerOffset = ((y + (subY * 2)) * SCREEN_WIDTH * 3) + (x * 3);
 
-            currentBg.add(buffer[bgOffset], buffer[bgOffset + 1], buffer[bgOffset + 2]);
-            currentFg.add(buffer[fgOffset], buffer[fgOffset + 1], buffer[fgOffset + 2]);
+            currentUpper.add(buffer[upperOffset], buffer[upperOffset + 1], buffer[upperOffset + 2]);
+            currentLower.add(buffer[lowerOffset], buffer[lowerOffset + 1], buffer[lowerOffset + 2]);
           }
         }
 
-        currentBg.rightShift(2);
-        currentFg.rightShift(2);
+        currentUpper.shiftRight(2);
+        currentLower.shiftRight(2);
 
-        // tslint:disable-next-line:max-line-length
-        if (!currentBg.equals(lastBg)) {
-          currentBg.writeBg();
-          lastBg.update(currentBg.r, currentBg.g, currentBg.b);
+        if (!currentUpper.equals(lastUpper)) {
+          currentUpper.writeToBackground();
+          lastUpper.update(currentUpper.r, currentUpper.g, currentUpper.b);
         }
 
-        // tslint:disable-next-line:max-line-length
-        if (!currentFg.equals(lastFg)) {
-          currentFg.writeFg();
-          lastFg.update(currentFg.r, currentFg.g, currentFg.b);
+        if (!currentLower.equals(lastLower)) {
+          currentLower.writeToForeground();
+          lastLower.update(currentLower.r, currentLower.g, currentLower.b);
         }
 
         process.stdout.write(LOWER_HALF_BLOCK);
@@ -86,9 +84,7 @@ class RgbColor {
   public b: number;
 
   public constructor(r: number, g: number, b: number) {
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    this.update(r, g, b);
   }
 
   public update(r: number, g: number, b: number) {
@@ -97,19 +93,13 @@ class RgbColor {
     this.b = b;
   }
 
-  public reset() {
-    this.r = 0;
-    this.g = 0;
-    this.b = 0;
-  }
-
   public add(r: number, g: number, b: number) {
     this.r += r;
     this.g += g;
     this.b += b;
   }
 
-  public rightShift(offset: number) {
+  public shiftRight(offset: number) {
     this.r >>= offset;
     this.g >>= offset;
     this.b >>= offset;
@@ -121,7 +111,7 @@ class RgbColor {
       && (other.b === this.b);
   }
 
-  public writeFg() {
+  public writeToForeground() {
     process.stdout.write(ansiStyles.color[COLOR_LEVEL_MAP[COLOR_LEVEL]].rgb(
       this.r,
       this.g,
@@ -129,7 +119,7 @@ class RgbColor {
     ));
   }
 
-  public writeBg() {
+  public writeToBackground() {
     process.stdout.write(ansiStyles.bgColor[COLOR_LEVEL_MAP[COLOR_LEVEL]].rgb(
       this.r,
       this.g,
